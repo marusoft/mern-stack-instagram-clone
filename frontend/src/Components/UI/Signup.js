d8b11265f918109cf2117a6d923d23a72c2e3371
marusoft <alimi.kmaruf@gmail.com>
2020-08-18 11:08:29 +0100

    integrate ui signup with the backend

--- frontend/src/Components/UI/Signup.js
+++ frontend/src/Components/UI/Signup.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const createUser = async () => {
+    const newUser = await fetch("/api/v1/signup", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "",
+        password: "",
+        email: "",
+      }),
+    });
+    const data = newUser.json();
+    console.log("data", data);
+  };
+
   return (
     <div className="login-card">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-        <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          onClick={() => createUser()}
+        >
           Signup
         </button>
         <h5>
