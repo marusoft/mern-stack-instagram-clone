ecc3010cd9266f9e7ad62e9c6e84ca14eb48cacb
marusoft <alimi.kmaruf@gmail.com>
2020-09-12 12:51:44 +0100

    integrated frontend and backend

--- frontend/src/Components/UI/CreatePost.js
+++ frontend/src/Components/UI/CreatePost.js
@@ -1,28 +1,111 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import M from "materialize-css";
+import { useHistory } from "react-router-dom";
 
 const CreatePost = () => {
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
+
+
+  const getUsersPost = async () => {
+    try {
+      const createUserPost = await fetch("/api/v1/createpost", {
+         method: "post",
+         headers: {
+           "Content-Type": "application/json",
+           "Authorization": "Bearer "+localStorage.getItem('jwt')
+         },
+         body: JSON.stringify({
+           title,
+           body,
+           pic: url,
+         }),
+       });
+       const data = await createUserPost.json();
+       if (data.error) {
+         M.toast({ html: data.error, classes: "#b71c1c red darken-4" });
+       } else {
+         M.toast({ html: data.message, classes: "#43a047 green darken-1" });
+         history.push("/");
+       }
+     } catch (error) {
+       console.log(error);
+     }
+  }
+
+  useEffect(() => {
+    if (url) {
+      getUsersPost();
+    }
+   
+  }, [url])
+
+  const uploadDetails = async () => {
+    try {
+      const data = new FormData();
+      data.append("file", image);
+      data.append("upload_preset", "instag-clone");
+      data.append("cloud_name", "marusofteamwork");
+
+      const createUpload = await fetch(
+        "https://api.cloudinary.com/v1_1/marusofteamwork/image/upload",
+        {
+          method: "post",
+          body: data,
+        }
+      );
+      const uploadData = await createUpload.json();
+      setUrl(uploadData.url);
+    } catch (err) {
+      console.log(err);
+    }
+
+  };
+
   return (
-    <div className="card input-field" 
-    style={{
-      margin: '30px auto',
-      maxWidth: '500px',
-      padding: '20px',
-      textAlign: 'center'
-    }}>
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="body" />
-      <div class="file-field input-field">
-        <div class="btn #64b5f6 blue darken-1">
+    <div
+      className="card input-field"
+      style={{
+        margin: "30px auto",
+        maxWidth: "500px",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      <div className="file-field input-field">
+        <div className="btn #64b5f6 blue darken-1">
           <span>Upload Photo</span>
-          <input type="file" />
+          <input type="file" onChange={(e) => setImage(e.target.files[0])} />
         </div>
-        <div class="file-path-wrapper">
-          <input class="file-path validate" type="text" placeholder="image"/>
+        <div className="file-path-wrapper">
+          <input
+            className="file-path validate"
+            type="text"
+            placeholder="image"
+          />
         </div>
       </div>
-      <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
-          Submit Post
-        </button>
+      <button
+        className="btn waves-effect waves-light #64b5f6 blue darken-1"
+        onClick={() => uploadDetails()}
+      >
+        Submit Post
+      </button>
     </div>
   );
 };
--- frontend/src/Components/UI/Signin.js
+++ frontend/src/Components/UI/Signin.js
@@ -1,14 +1,69 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
 const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const signinUser = async () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      M.toast({
+        html: "invalid email format",
+        classes: "#b71c1c red darken-4",
+      });
+      return;
+    }
+    try {
+      const userSigninData = await fetch("/api/v1/signin", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password,
+          email,
+        }),
+      });
+      const data = await userSigninData.json();
+      console.log('info',data)
+      if (data.error) {
+        M.toast({ html: data.error, classes: "#b71c1c red darken-4" });
+      } else {
+        localStorage.setItem('jwt', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
+        M.toast({ html: data.message, classes: "#43a047 green darken-1" });
+        history.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="login-card">
       <div className="card auth-card input-field">
         <h2>Instagram</h2>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-        <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          onClick={() => signinUser()}
+        >
           Signin
         </button>
         <h5>
