e0c9876dff9301cd96b64709b378820d7b94906e
marusoft <alimi.kmaruf@gmail.com>
2020-09-24 11:27:01 +0100

    implement logout functionality

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,14 +1,25 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { UserContext } from '../App';
 
 const NavBar = () => {
   const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
   const renderList = () => {
     if(state){
       return [
         <li><Link to="/profile">Profile</Link></li>,
-        <li><Link to="/createpost">Create Post</Link></li>
+        <li><Link to="/createpost">Create Post</Link></li>,
+        <li> <button
+        className="btn #b71c1c red darken-4"
+        onClick={() => {
+          localStorage.clear()
+          dispatch({type:"CLEAR"})
+          history.push("/signin")
+        }}
+      >
+        Logout
+      </button></li>
       ]
     }
     else{
