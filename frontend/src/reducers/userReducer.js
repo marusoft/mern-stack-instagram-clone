ff63098ec7ec68a1748ecc53ba258e2bb4eee847
marusoft <alimi.kmaruf@gmail.com>
2020-09-24 10:42:14 +0100

    change equality operator

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,7 +1,7 @@
 export const initialState = null;
 
 export const reducer = (state, action) => {
-  if(action.type == 'USER'){
+  if(action.type === 'USER'){
     return action.payload;
   }
   return state;
