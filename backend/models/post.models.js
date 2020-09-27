0c68c0422b8bf227930f41f1c1bbdc7a70016f2b
marusoft <alimi.kmaruf@gmail.com>
2020-09-27 17:33:32 +0100

    implement likes and unlikes features

--- backend/models/post.models.js
+++ backend/models/post.models.js
@@ -15,6 +15,10 @@ const postSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  likes: [{
+    type: ObjectId,
+    ref: 'User',
+  }],
   postedBy: {
     type: ObjectId,
     ref: 'User',
