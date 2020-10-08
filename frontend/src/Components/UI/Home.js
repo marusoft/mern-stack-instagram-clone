d5fef37692902cf566396b4550419e72794fb5f1
marusoft <alimi.kmaruf@gmail.com>
2020-10-08 14:25:37 +0100

    implement comment feature on the frontend

--- frontend/src/Components/UI/Home.js
+++ frontend/src/Components/UI/Home.js
@@ -67,14 +67,42 @@ const Home = () => {
         }),
       });
       const unLikePostResult = await unLikeUserPost.json();
-      const updateLikeData = data.map((item) => {
+      const updateUnLikeData = data.map((item) => {
         if (item._id === unLikePostResult._id) {
           return unLikePostResult;
         } else {
           return item;
         }
       });
-      setData(updateLikeData);
+      setData(updateUnLikeData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //make post comment
+  const createPostComment = async (text, id) => {
+    try {
+      const postComment = await fetch("/api/v1/commentpost", {
+        method: "put",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+        body: JSON.stringify({
+          text,
+          postId: id,
+        }),
+      });
+      const commentPostResult = await postComment.json();
+      const updateCommentData = data.map((item) => {
+        if (item._id === commentPostResult._id) {
+          return commentPostResult;
+        } else {
+          return item;
+        }
+      });
+      setData(updateCommentData);
     } catch (error) {
       console.log(error);
     }
@@ -117,7 +145,19 @@ const Home = () => {
               <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
-              <input type="text" placeholder="add a comment" />
+              {item.comments.map((userComment) => {
+                return (
+                  <h6 key={userComment._id}><span style={{ fontWeight: "500" }}>{userComment.postedBy.name}</span> {userComment.text}</h6>
+                );
+              })}
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  createPostComment(e.target[0].value, item._id);
+                }}
+              >
+                <input type="text" placeholder="add a comment" />
+              </form>
             </div>
           </div>
         );
