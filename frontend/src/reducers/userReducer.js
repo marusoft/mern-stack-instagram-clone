b2ea2e65550615de4a1badec57adb9f79b1e7443
marusoft <alimi.kmaruf@gmail.com>
2020-11-21 16:28:55 +0100

    implement client follow and unfollow features

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -7,5 +7,12 @@ export const reducer = (state, action) => {
   if(action.type === 'CLEAR'){
     return null;
   }
+  if (action.type === 'UPDATE') {
+    return {
+      ...state,
+      follower: action.payload.followers,
+      following: action.payload.following
+    }
+  }
   return state;
 }
